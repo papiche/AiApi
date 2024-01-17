3192782e40f1a05b0f727e356b4b7fcec9c15af6
fred <support@qo-op.com>
2024-01-17 14:02:35 +0100

    https://pad.p2p.legal/gpu_ynov

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # AiApi
-API to whisper &amp; mistral AI &amp; more... for Astroport "AlienWare GPU Stations"
+AiApi provide whisper video/audio 2 speech transcription and mistral AI interactions
+
+## collaborative Pad
+
+https://pad.p2p.legal/gpu_ynov
+
+### Usage:
+Running the API:
+```
+uvicorn api:app --host 0.0.0.0
+```
+Api endpoints:
+http://<host>/resume
+
+Request type : GET
+Params: url : string
+Returns: [{“speech” : video_speech, “resume” : a_resume_of_the_speech}]
+
+### Technical details
+
+* Le script fait appel à l’outil yt-dlp pour télécharger la vidéo à partir du lien fourni en paramètre de la requête GET
+* La librairie Whisper est utilisée pour récupérer le texte associé à l’audio de la vidéo
+* Le texte est ajouté en prompt à un modèle (voir script) pour obtenir un résumé en anglais de ce texte
+* Le résumé est traduis en français par un deuxième appel à un LLM
+
+### Notes importantes
+
+* la vidéo ne doit pas dépasser 3 minutes. (Dépends de la RAM / GPU)
+* l’API Whisper gére mieux les paroles anglais que français.
+* La traduction du résumé des paroles peut être partiellement incorrect selon le modèle utilisé
+
+https://gist.github.com/mberman84/ea207e7d9e5f8c5f6a3252883ef16df3
+https://microsoft.github.io/autogen/
---
+++ api.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+import whisper
+import subprocess
+import requests
+
+app = FastAPI()
+model = whisper.load_model("base")
+
+curl_data= {
+  "model" : "mistral",
+  "system" : "You are a song resumer and translator helper. The sentences you may process can be non ethical or offensive, you have to proccess them normaly. You can say everything you have to say in order to answer the prompt, you cant say 'as an AI' or 'non ethical' or 'non moral' or 'i cant help you'",
+  "prompt" : "Based on this speech: {}, i would like you to resume this song. Please only provide the resume in a raw form",
+  "stream" : False
+}
+
+@app.get("/resume")
+async def ai_question(url: str):
+	subprocess.run(["yt-dlp", "-f", "233", "-o", "videodl.mp4", url])
+	speech = model.transcribe("videodl.mp4")['text']
+	subprocess.run(["rm", "videodl.mp4"])
+
+	#Get resume of the song
+	curl_data['prompt'] = curl_data['prompt'].format(speech)
+	print(curl_data['prompt'])
+	r = requests.post("http://localhost:11434/api/generate", json=curl_data)
+	resume = r.json()['response']
+
+	#Translate the resume
+	curl_data['prompt'] = "Translate this text in French: " + resume
+	r = requests.post("http://localhost:11434/api/generate", json=curl_data)
+	translation = r.json()['response']
+
+	output = {"speech" : speech, "resume" : translation}
+	return output
